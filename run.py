ac46ee6309efd7fbafa0d37e80df8d253f1c9bec
id10tothe9 <id10tothe9@googlemail.com>
2023-08-26 18:59:28 +0000

    Add get_user_inupt() to handle input requests
    This functions prints out the input request to the user
    and checks if their input is valid according to specified
    criteria, if not it requests a new input, otherwise
    returns the valid user_input.

--- run.py
+++ run.py
@@ -10,8 +10,25 @@ def main_menu_message():
   3. Display calculated values\n"""
 
 
-def main_menu():
+def get_user_input(message, requirements_list):
+  """
+  Handle all input requests: print out message to user, check
+  if their answer satisfies all requirements by calling the check_input
+  function. Return user_input if user answer is valid.
+  """
+  while True:
+    user_input = input(message)
+    error_message = check_input(user_input, requirements_list)
+    if error_message:
+      print(error_message)
+    else:
+      break
+  return user_input
 
+
+def main_menu():
+  message = main_menu_message()
+  user_input = get_user_input(message, ['integer', (1, 3)])
 
 
 def main():
