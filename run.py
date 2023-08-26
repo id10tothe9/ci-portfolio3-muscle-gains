a618029dccc5dac3a684fc6745774c6521ba6fe7
id10tothe9 <id10tothe9@googlemail.com>
2023-08-26 18:28:12 +0000

    Add basic welcome message and main menu options

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 
+def welcome_message():
+  return "Welcome!\n"
 
 
+def main_menu_message():
+  return """Choose an option:
+  1. Create a new training plan
+  2. Display current training plan
+  3. Display calculated values\n"""
+
+
+def main_menu():
 
 
 
