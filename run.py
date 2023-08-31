e12a7ce6bffa3c6ec5cbf3744dbc4461241a3d3d
id10tothe9 <id10tothe9@googlemail.com>
2023-08-31 08:57:04 +0000

    Modify user prompt for cadence input

--- run.py
+++ run.py
@@ -358,7 +358,7 @@ class Exercise():
 
     # get the cadence values (a set of three numbers) in seconds
     def get_cadence(self):
-        message = 'Cadence: enter the duration of the contraction, pause and extension\nin that order as comma (or space) separated numbers:\ne.g. 2, 0, 4\n\nYou can skip this value by pressing enter instead.'
+        message = 'Cadence: enter the duration of the contraction, pause and extension\n of the muscle in that order as comma (or space) separated numbers:\ne.g. 2, 0, 4\n\nYou can skip this value by pressing enter instead.'
         cadence = get_user_input(message, ['can skip', 'cadence'])
         if cadence != '':
             self.cadence = cadence
